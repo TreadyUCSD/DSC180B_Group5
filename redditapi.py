f9203580cb38dd276390c4ddf9a13f169af17af4
TreadyUCSD <tready@ucsd.edu>
2022-01-19 11:01:02 -0800

    Now Accepts subs as args
    The file now runs as a script that accepts subreddits as arguments

--- redditapi.py
+++ redditapi.py
@@ -1,13 +1,13 @@
+#!/usr/bin/python
 from tracemalloc import start
 import requests
 import pandas as pd
 from datetime import datetime
 import json
 import os
+import sys
 
-startDate = 1577836800
-endDate = 1609459199
-subredditList = []
+
 
 def df_from_response(res):
     df = pd.DataFrame()
@@ -29,59 +29,69 @@ def df_from_response(res):
 
     return df
 
-client_auth = requests.auth.HTTPBasicAuth('personal use token', 'secret token')
-data = {
-    'grant_type': 'password',
-    'username': 'username',
-    'password': 'password'
-}
+def main(targets):
 
-headers = {'User-Agent': 'myBot/0.0.1'}
-res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=client_auth, data=data, headers=headers)
+    startDate = 1577836800
+    endDate = 1609459199
+    subredditList = targets
 
-TOKEN = f"bearer {res.json()['access_token']}"
-headers = {**headers, **{'Authorization': TOKEN}}
+    client_auth = requests.auth.HTTPBasicAuth('personal use token', 'secret token')
+    data = {
+        'grant_type': 'password',
+        'username': 'username',
+        'password': 'password'
+    }
+
+    headers = {'User-Agent': 'myBot/0.0.1'}
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=client_auth, data=data, headers=headers)
+
+    TOKEN = f"bearer {res.json()['access_token']}"
+    headers = {**headers, **{'Authorization': TOKEN}}
 
 
-data = pd.DataFrame()
-params = {'size': 500}
-os.chdir('..')
-cur_dir = os.getcwd()
-os.chdir(cur_dir + '/data')
+    data = pd.DataFrame()
+    params = {'size': 500}
+    os.chdir('..')
+    cur_dir = os.getcwd()
+    os.chdir(cur_dir + '/data')
 
 
-for sub in subredditList:
-    filename = str(sub) + '_posts.jsonl'
-    with open(filename, 'w') as f:
-        params['subreddit'] = sub
-        params['after'] = startDate
-        while params['after'] <= endDate:
-            res = requests.get('https://api.pushshift.io/reddit/search/submission/', params=params)
-            new_df = df_from_response(res)
-            row = new_df.iloc[len(new_df)-1]
-            params['after'] = row['created_utc']
-            data = data.append(new_df, ignore_index=True)
-            length = data.shape[0]
-            if length >= 100000:
-                data_json = data.to_json(orient="records")
-                parsed = json.loads(data_json)
-                lines = json.dumps(parsed)
-                for line in lines:
-                    f.write(line)
-                    f.write('\n')
-                f.flush()
-                os.fsync(f.fileno())
-                data = pd.DataFrame()
-        data_json = data.to_json(orient="records")
-        parsed = json.loads(data_json)
-        lines = json.dumps(parsed)
-        for line in lines:
-            f.write(line)
-            f.write('\n')
-        f.flush()
-        os.fsync(f.fileno())
-        data = pd.DataFrame()
+    for sub in subredditList:
+        filename = str(sub) + '_posts.jsonl'
+        with open(filename, 'w') as f:
+            params['subreddit'] = sub
+            params['after'] = startDate
+            while params['after'] <= endDate:
+                res = requests.get('https://api.pushshift.io/reddit/search/submission/', params=params)
+                new_df = df_from_response(res)
+                row = new_df.iloc[len(new_df)-1]
+                params['after'] = row['created_utc']
+                data = data.append(new_df, ignore_index=True)
+                length = data.shape[0]
+                if length >= 100000:
+                    data_json = data.to_json(orient="records")
+                    parsed = json.loads(data_json)
+                    lines = json.dumps(parsed)
+                    for line in lines:
+                        f.write(line)
+                        f.write('\n')
+                    f.flush()
+                    os.fsync(f.fileno())
+                    data = pd.DataFrame()
+            data_json = data.to_json(orient="records")
+            parsed = json.loads(data_json)
+            lines = json.dumps(parsed)
+            for line in lines:
+                f.write(line)
+                f.write('\n')
+            f.flush()
+            os.fsync(f.fileno())
+            data = pd.DataFrame()
+
+if __name__ == '__main__':
+    targets = sys.argv[1:]
+    main(targets)
                 
 
 
