3e2cfeca42f02d1051d1bffb3daefebd6a2b7ed7
TreadyUCSD <tready@ucsd.edu>
2022-01-19 11:44:53 -0800

    Updated writing
    Updated so that the file is written in the correct format

--- redditapi.py
+++ redditapi.py
@@ -24,7 +24,7 @@ def df_from_response(res, endDate = 1609459199):
                 'id': post['id'],
                 'full_link': post['full_link'],
                 'url': post['url'],
-                'url_overridden_by_dest': post['url_overridden_by_dest']
+                #'url_overridden_by_dest': post['url_overridden_by_dest']
             }, ignore_index=True)
 
     return df
@@ -65,25 +65,26 @@ def main(targets):
             while params['after'] <= endDate:
                 res = requests.get('https://api.pushshift.io/reddit/search/submission/', params=params)
                 new_df = df_from_response(res)
-                row = new_df.iloc[len(new_df)-1]
-                params['after'] = row['created_utc']
-                data = data.append(new_df, ignore_index=True)
-                length = data.shape[0]
-                if length >= 100000:
-                    data_json = data.to_json(orient="records")
-                    parsed = json.loads(data_json)
-                    lines = json.dumps(parsed)
-                    for line in lines:
-                        f.write(line)
-                        f.write('\n')
-                    f.flush()
-                    os.fsync(f.fileno())
-                    data = pd.DataFrame()
+                if new_df.shape[0] > 0:
+                    row = new_df.iloc[len(new_df)-1]
+                    params['after'] = int(row['created_utc'])
+                    data = data.append(new_df, ignore_index=True)
+                    length = data.shape[0]
+                    if length >= 100000:
+                        data_json = data.to_json(orient="records")
+                        parsed = json.loads(data_json)
+                        lines = json.dumps(parsed)
+                        for line in lines:
+                            f.write(line)
+                            f.write('\n')
+                        f.flush()
+                        os.fsync(f.fileno())
+                        data = pd.DataFrame()
+                else: break
             data_json = data.to_json(orient="records")
             parsed = json.loads(data_json)
-            lines = json.dumps(parsed)
-            for line in lines:
-                f.write(line)
+            for line in parsed:
+                f.write(json.dumps(line))
                 f.write('\n')
             f.flush()
             os.fsync(f.fileno())
